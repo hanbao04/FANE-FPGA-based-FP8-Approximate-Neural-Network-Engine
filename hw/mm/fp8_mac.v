003ba142e3b910ee5fc30f148ba7fe456e893782
han-bao <h.bao1004@icloud.com>
2026-05-12 21:32:48 +0200

    Bug-Fixed: LUT change in addmul for edge case

--- hw/mm/fp8_mac.v
+++ hw/mm/fp8_mac.v
@@ -1,153 +1,101 @@
 `timescale 1ns / 1ps
 
-module fane_mac #(
-    parameter EXP_WIDTH  = 4,
-    parameter MANT_WIDTH = 3,
-    parameter INPUT_DELAY = 0
-) (
-    input clk,
-    input rst_n,
-    input valid_in,
-    input [7:0] mul_a,
-    input [7:0] mul_b,
-    input [7:0] cascade_sum_in,
-    output [7:0] acc_out,
-    output [7:0] cascade_mula_out,
-    output [7:0] cascade_mulb_out,
-    output valid_out
+module fpmac#(
+    parameter EXP_WIDTH     = 4,   
+    parameter MANT_WIDTH    = 3,    
+    parameter NUM_INREG     = 1
+)(
+    input   wire                clk     ,
+    input   wire                rst_n   ,
+    input   wire                en      ,
+    input   wire                en_1    ,
+    input   wire                en_2    ,
+
+    input   wire    [7:0]       mul_a   ,            
+    input   wire    [7:0]       mul_b   ,             
+    input   wire    [7:0]       sum_in  ,
+
+    output  reg     [7:0]       acc_out ,
+    output  wire    [7:0]       PCOUT   
 );
 
-reg [7:0] mul_a_r;
-reg [7:0] mul_b_r;
-reg [7:0] mul_a_r1;
-reg [7:0] mul_a_r2;
-reg [7:0] mul_a_r3;
-reg [7:0] mul_a_r4;
-reg [7:0] mul_b_r1;
-reg [7:0] mul_b_r2;
-reg [7:0] mul_b_r3;
-reg [7:0] mul_b_r4;
-reg [7:0] sum_r0;
-reg [7:0] sum_r1;
-reg [7:0] sum_r2;
-reg [7:0] fp8_product_r;
-reg [7:0] acc_out_r;
-reg valid_r0;
-reg valid_r1;
-reg valid_r2;
-reg valid_r3;
-reg valid_r4;
-reg [7:0] mul_a_hist [0:INPUT_DELAY];
-reg [7:0] mul_b_hist [0:INPUT_DELAY];
-integer i;
+wire    [7:0]   prod_out    ;
+reg     [7:0]   sum_r0      ;
+reg     [7:0]   sum_r1      ;
+reg     [7:0]   sum_r2      ;
+reg     [7:0]   ain_r0      ;
+reg     [7:0]   bin_r0      ;
+wire    [7:0]   acc_res     ;
 
-wire [7:0] fp8_product;
-wire [7:0] sum_result;
+generate
+    if (NUM_INREG == 1) begin : signle_inreg
+        (* dont_touch = "true" *) addmul U_FP8_ADDMUL(
+            .sys_clk (clk        ),
+            .rst_n   (rst_n      ),
+            .en      (en_2       ),
+            .w_fp8_x (mul_a      ),
+            .w_fp8_y (mul_b      ),
+            .product (prod_out   )
+        );
+
+        (* dont_touch = "true" *) fpadder#(
+            .e      (EXP_WIDTH      ),
+            .m      (MANT_WIDTH     )
+        ) U_FP8_ADDER(
+            .a      (sum_r1         ),
+            .b      (prod_out       ),
+            .result (acc_res        )
+        );
+    end else if (NUM_INREG == 2) begin : dou_inreg
+        (* dont_touch = "true" *) addmul U_FP8_ADDMUL(
+            .sys_clk (clk        ),
+            .rst_n   (rst_n      ),
+            .en      (en_2       ),
+            .w_fp8_x (ain_r0     ),
+            .w_fp8_y (bin_r0     ),
+            .product (prod_out   )
+        );
+
+        (* dont_touch = "true" *) fpadder#(
+            .e      (EXP_WIDTH      ),
+            .m      (MANT_WIDTH     )
+        ) U_FP8_ADDER(
+            .a      (sum_r2         ),
+            .b      (prod_out       ),
+            .result (acc_res        )
+        );
+        always @(posedge clk or negedge rst_n) begin
+            if(!rst_n) begin
+                sum_r2 <= 8'd0;
+                ain_r0 <= 8'b0;
+                bin_r0 <= 8'b0;
+            end else begin
+                if (en_1) begin
+                    sum_r2 <= sum_r1;
+                    ain_r0 <= mul_a ;
+                    bin_r0 <= mul_b ;
+                end
+            end
+        end
+    end
+endgenerate
+
+
 
 always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        for (i = 0; i <= INPUT_DELAY; i = i + 1) begin
-            mul_a_hist[i] <= 8'd0;
-            mul_b_hist[i] <= 8'd0;
-        end
-        mul_a_r <= 8'd0;
-        mul_b_r <= 8'd0;
-        mul_a_r1 <= 8'd0;
-        mul_a_r2 <= 8'd0;
-        mul_a_r3 <= 8'd0;
-        mul_a_r4 <= 8'd0;
-        mul_b_r1 <= 8'd0;
-        mul_b_r2 <= 8'd0;
-        mul_b_r3 <= 8'd0;
-        mul_b_r4 <= 8'd0;
-        sum_r0 <= 8'd0;
-        sum_r1 <= 8'd0;
-        sum_r2 <= 8'd0;
-        fp8_product_r <= 8'd0;
-        acc_out_r <= 8'd0;
-        valid_r0 <= 1'b0;
-        valid_r1 <= 1'b0;
-        valid_r2 <= 1'b0;
-        valid_r3 <= 1'b0;
-        valid_r4 <= 1'b0;
+    if(!rst_n) begin
+        acc_out <= 8'd0;
+        sum_r0  <= 8'b0;
+        sum_r1  <= 8'b0;
     end else begin
-        mul_a_hist[0] <= mul_a;
-        mul_b_hist[0] <= mul_b;
-        for (i = 1; i <= INPUT_DELAY; i = i + 1) begin
-            mul_a_hist[i] <= mul_a_hist[i-1];
-            mul_b_hist[i] <= mul_b_hist[i-1];
-        end
-
-        valid_r0 <= valid_in;
-        valid_r1 <= valid_r0;
-        valid_r2 <= valid_r1;
-        valid_r3 <= valid_r2;
-        valid_r4 <= valid_r3;
-
-        if (valid_in) begin
-            mul_a_r <= mul_a_hist[INPUT_DELAY];
-            mul_b_r <= mul_b_hist[INPUT_DELAY];
-            sum_r0 <= cascade_sum_in;
-        end
-
-        if (valid_r0) begin
-            mul_a_r1 <= mul_a_r;
-            mul_b_r1 <= mul_b_r;
-        end
-
-        if (valid_r1) begin
-            mul_a_r2 <= mul_a_r1;
-            mul_b_r2 <= mul_b_r1;
-        end
-
-        if (valid_r2) begin
-            mul_a_r3 <= mul_a_r2;
-            mul_b_r3 <= mul_b_r2;
-        end
-
-        if (valid_r3) begin
-            mul_a_r4 <= mul_a_r3;
-            mul_b_r4 <= mul_b_r3;
-        end
-
-        if (valid_r0) begin
-            sum_r1 <= sum_r0;
-        end
-
-        if (valid_r1) begin
-            sum_r2 <= sum_r1;
-            fp8_product_r <= fp8_product;
-        end
-
-        if (valid_r3) begin
-            acc_out_r <= sum_result;
+        if (en) begin
+            acc_out <= acc_res;
+            sum_r0  <= sum_in ;
+            sum_r1  <= sum_r0 ;
         end
     end
 end
 
-(* dont_touch = "true" *) fp8_addmul #(
-    .e(EXP_WIDTH),
-    .m(MANT_WIDTH)
-) u_fp8_addmul (
-    .sys_clk(clk),
-    .rst_n(rst_n),
-    .w_fp8_x(mul_a_r),
-    .w_fp8_y(mul_b_r),
-    .product(fp8_product)
-);
-
-(* dont_touch = "true" *) fp8_adder #(
-    .e(EXP_WIDTH),
-    .m(MANT_WIDTH)
-) u_fp8_adder (
-    .a(sum_r2),
-    .b(fp8_product_r),
-    .result(sum_result)
-);
-
-assign acc_out = acc_out_r;
-assign cascade_mula_out = mul_a_r4;
-assign cascade_mulb_out = mul_b_r4;
-assign valid_out = valid_r4;
+assign PCOUT = acc_out;
 
 endmodule
